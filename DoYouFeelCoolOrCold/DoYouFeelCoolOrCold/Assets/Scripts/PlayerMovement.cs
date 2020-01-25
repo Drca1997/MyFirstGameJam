774ebdecdc3a676bbd4fc1afcff50c83bf6b66c7
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-01-25 16:50:14 +0000

    Tentar faze-lo correr

---
+++ DoYouFeelCoolOrCold/DoYouFeelCoolOrCold/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocidade_normal = 5f;
+    public float run_modifier = 2f; //multiplica-se velocidade normal por este valor quando personagem esta a correr
+    public Rigidbody2D rb;
+    Vector2 movimento;
+
+    public void Update()
+    {
+       
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("A correr...");
+            movimento.x *= run_modifier;
+            movimento.y *= run_modifier;
+
+        }
+    }
+
+
+    public void FixedUpdate()
+    {
+       
+        rb.MovePosition(rb.position + movimento * velocidade_normal * Time.fixedDeltaTime);
+        
+    }
+
+}
