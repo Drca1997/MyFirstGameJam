cdef527a29b4af22069e8213e0864c5811ba625d
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-01-25 15:57:55 +0000

    Movimento

---
+++ DoYouFeelCoolOrCold/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocidade = 5f;
+    public Rigidbody2D rb;
+    Vector2 movimento;
+
+    public void Update()
+    {
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    public void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movimento * velocidade * Time.fixedDeltaTime);
+    }
+
+}
