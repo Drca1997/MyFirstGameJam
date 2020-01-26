d55865615d09411941a4e0400c69575de9b6190b
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-01-26 19:16:13 +0000

    stamina bar

--- DoYouFeelCoolOrCold/Assets/Scripts/PlayerMovement.cs
+++ DoYouFeelCoolOrCold/Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float velocidade = 5f;
-    public float run_modifier = 2f;
+    [SerializeField] private StaminaBar staminaBar;
+
+
+    [SerializeField] private float velocidade;
+    [SerializeField] private float run_modifier;
+    [SerializeField] private float stamina_run_modifier;
+    [SerializeField]private float stamina_regeneration_ratio;
     public Rigidbody2D rb;
     Vector2 movimento;
+    private bool is_running;
+    [SerializeField]private float stamina;
 
+
+   
     public void Update()
     {
+        stamina = staminaBar.transform.Find("Bar").localScale.x;
+        is_running = false;
         movimento.x = Input.GetAxisRaw("Horizontal");
         movimento.y = Input.GetAxisRaw("Vertical");
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            Debug.Log("A correr...");
-            movimento.x *= run_modifier;
-            movimento.y *= run_modifier;
 
+            if (stamina > 0.0f)
+            {
+                Debug.Log("A correr...");
+                movimento.x *= run_modifier;
+                movimento.y *= run_modifier;
+                is_running = true;
+            }
+            
         }
-    }
+        
+    }   
 
 
     public void FixedUpdate()
     {
+        StaminaManager();
         rb.MovePosition(rb.position + movimento * velocidade * Time.fixedDeltaTime);
+       
     }
 
+    public void StaminaManager()
+    {
+        if (is_running)
+        {
+            staminaBar.SetSize(stamina - stamina_run_modifier);
+        }
+        else
+        {
+            if (stamina < 1f)
+            {
+                staminaBar.SetSize(stamina + stamina_run_modifier / stamina_regeneration_ratio);
+
+            }
+
+        }
+    }
 }
---
+++ DoYouFeelCoolOrCold/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBar : MonoBehaviour
+{
+    private Transform bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float barsize)
+    {
+        bar.localScale = new Vector2(barsize, 1f);
+    }
+}
