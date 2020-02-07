68e855ff817574cca699a3002fc6571721585295
tgcunha999 <33125227+tgcunha999@users.noreply.github.com>
2020-02-07 00:24:52 +0000

    nivel dos obstaculos

--- DoYouFeelCoolOrCold/Assets/Scripts/Help.cs
+++ DoYouFeelCoolOrCold/Assets/Scripts/Help.cs
@@ -18,7 +18,9 @@ public class Help : MonoBehaviour
                                    "Sometimes, you'll have to get a cooling boost with a drink to be able to get to the next shadow.\nBe sure not to miss it or it could be really bad!",
                                    "And other times, one boost isn't enough!",
                                    "Be sure to fully cool yourself before continuing,\nyou never know when you'll need to walk/run a big distance!",
-                                   "Be sure to fully cool yourself before continuing,\nyou never know when you'll need to walk/run a big distance!"};
+                                   "Be sure to fully cool yourself before continuing,\nyou never know when you'll need to walk/run a big distance!",
+                                   "Damn, beach sure is crowdy today.\nBetter not to touch these people, they're hot from being in the sun for too long.",
+                                   "There are a lot of drinks there; maybe i shouldn't drink all of them at once.."};
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
