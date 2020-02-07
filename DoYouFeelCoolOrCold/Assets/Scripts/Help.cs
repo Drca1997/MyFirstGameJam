0aa72c3f43e226a22b734ef369319eb3874b40f9
tgcunha999 <33125227+tgcunha999@users.noreply.github.com>
2020-02-07 12:41:05 +0000

    tutorial e obstaculos 99%

--- DoYouFeelCoolOrCold/Assets/Scripts/Help.cs
+++ DoYouFeelCoolOrCold/Assets/Scripts/Help.cs
@@ -6,21 +6,20 @@ public class Help : MonoBehaviour
 {
     private GameObject player;
     private string[] helpTexts;
-    private int time_used;
     public TMPro.TextMeshProUGUI helpTextUI;
 
     private void Awake()
     {
-        time_used = 0;
         helpTexts = new string[] { "The sand is hot! Walk in the shadows to keep cold!\n Run pressing shift and the direction key.",
-                                   "Careful with walking big distances\n some paths lead nowhere!",
+                                   "Careful with walking big distances\n some paths lead nowhere!\nAnd what's with all these beach windbreakers?",
                                    "See? Nowhere! :(",
                                    "Sometimes, you'll have to get a cooling boost with a drink to be able to get to the next shadow.\nBe sure not to miss it or it could be really bad!",
                                    "And other times, one boost isn't enough!",
                                    "Be sure to fully cool yourself before continuing,\nyou never know when you'll need to walk/run a big distance!",
                                    "Be sure to fully cool yourself before continuing,\nyou never know when you'll need to walk/run a big distance!",
                                    "Damn, beach sure is crowdy today.\nBetter not to touch these people, they're hot from being in the sun for too long.",
-                                   "There are a lot of drinks there; maybe i shouldn't drink all of them at once.."};
+                                   "There are a lot of drinks there; maybe i shouldn't drink all of them at once..",
+                                   "The cavern are the end of the level, so when you enter them, you advance to the next level!"};
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
